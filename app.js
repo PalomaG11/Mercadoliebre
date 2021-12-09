9b5bb5722fd43bce8e36dc6b0b7226e98606b670
PalomaG11 <palomagras11@gmail.com>
2021-12-08 23:51:19 -0300

    actualizacion mercadoliebre

--- app.js
+++ app.js
@@ -1,30 +1,9 @@
-//
-// imports
-//
-const express = require('express');
-const path = require('path');
-const app = express();
+const express = require('express'); 
+ const app = express();
+ const path=require('path');
 
-//
-// constants
-//
-const port = process.env.PORT || 3000;
-const baseViewsPath = path.join(__dirname, 'views');
+ app.use(express.static('public'));
 
-//
-// middlewares
-//
-app.use(express.static('public'));
+ app.listen(3000, () => console.log ("servidor corriendo"));
 
-//
-// routes
-//
-app.get('/', (req, res) => res.sendFile(path.join(baseViewsPath, 'home.html')));
-
-// error pages
-app.get('/404', (req, res) => res.send('Error página no encontrada'));
-
-//
-// listen application
-//
-app.listen(port, () => console.log(`Servidor iniciado en el puerto http://localhost:${port}`));
+app.get('/', (req,res) =>res.sendFile(path.join(__dirname,'/views/home.html')));
