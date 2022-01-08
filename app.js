7b563c99f62448d5d76646295a80beb59d3d776a
Cristian Besada <cristian.besada@hotmail.com>
2022-01-08 01:24:10 -0300

    creacion de formularios

--- app.js
+++ app.js
@@ -7,3 +7,12 @@ const express = require('express');
  app.listen(3000, () => console.log ("servidor corriendo"));
 
 app.get('/', (req,res) =>res.sendFile(path.join(__dirname,'/views/home.html')));
+
+app.get('/register', (req,res) =>res.sendFile(path.join(__dirname,'/views/register.html')));
+
+app.post('/register', (req,res) =>res.sendFile(path.join(__dirname,'/views/home.html')));
+
+app.get('/login', (req,res) =>res.sendFile(path.join(__dirname,'/views/login.html')));
+
+app.post('/login', (req,res) =>res.sendFile(path.join(__dirname,'/views/home.html')));
+
